66e11e772a4e0b86a903383a00c2e48c53b70bdf
Proloy <xtrmproloy875@gmail.com>
2020-04-16 03:32:28 +0600

    Update done/New upload file

--- library_management_system.sql
+++ library_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2020 at 09:31 PM
+-- Generation Time: Apr 15, 2020 at 11:11 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -47,8 +47,8 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `book_name`, `book_image`, `book_author_name`, `book_publication_name`, `book_purchase_date`, `book_price`, `book_qty`, `available_qty`, `librarian_username`, `datetime`) VALUES
-(10, 'গনিত বিচিত্রা', '20200403122931.png', 'অজয় সরকার', 'জয়কলি পাবলিকেশন', '2020-04-03', '500', 150, 68, 'sujoy123', '2020-04-03 14:55:03'),
-(11, 'STATISTICS FOR BUSINESS AND ECONOMICS', '20200403010454.jpg', 'J S CHANDAN', 'J S CHANDAN', '2020-04-03', '350', 200, 148, 'sujoy123', '2020-04-03 14:55:03');
+(10, 'গনিত বিচিত্রা', '20200403122931.png', 'অজয় সরকার', 'জয়কলি পাবলিকেশন', '2020-04-03', '500', 150, 66, 'sujoy123', '2020-04-03 14:55:03'),
+(11, 'STATISTICS FOR BUSINESS AND ECONOMICS', '20200403010454.jpg', 'J S CHANDAN', 'J S CHANDAN', '2020-04-03', '350', 200, 146, 'sujoy123', '2020-04-03 14:55:03');
 
 -- --------------------------------------------------------
 
@@ -77,7 +77,11 @@ INSERT INTO `issue_books` (`id`, `student_id`, `book_id`, `book_issue_date`, `bo
 (5, 16, 10, '06-Apr-2020', '', '2020-04-06 15:14:01'),
 (6, 16, 11, '06-Apr-2020', '', '2020-04-06 15:15:11'),
 (7, 17, 10, '12-Apr-2020', '', '2020-04-12 15:52:49'),
-(8, 17, 11, '12-Apr-2020', '', '2020-04-12 15:53:00');
+(8, 17, 11, '12-Apr-2020', '', '2020-04-12 15:53:00'),
+(9, 18, 10, '15-Apr-2020', '', '2020-04-15 10:31:14'),
+(10, 18, 11, '15-Apr-2020', '', '2020-04-15 10:31:25'),
+(11, 24, 10, '15-Apr-2020', '', '2020-04-15 21:10:38'),
+(12, 24, 11, '15-Apr-2020', '', '2020-04-15 21:10:50');
 
 -- --------------------------------------------------------
 
@@ -91,8 +95,10 @@ CREATE TABLE `librarian` (
   `email` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
+  `token` varchar(50) NOT NULL,
   `mobile` varchar(50) NOT NULL,
   `image` varchar(50) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
   `datetime` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -100,8 +106,8 @@ CREATE TABLE `librarian` (
 -- Dumping data for table `librarian`
 --
 
-INSERT INTO `librarian` (`id`, `fname`, `email`, `username`, `password`, `mobile`, `image`, `datetime`) VALUES
-(5, 'sujoy das', 'sujoy15-5750@diu.edu.bd', 'sujoy123', 'sujoy1234', '01521110903', '20200412054216.jpg', '2020-04-12 15:42:16');
+INSERT INTO `librarian` (`id`, `fname`, `email`, `username`, `password`, `token`, `mobile`, `image`, `status`, `datetime`) VALUES
+(15, 'sujoy das', 'sujoy15-5750@diu.edu.bd', 'sujoy123', '12345678', '1aea3d3f23794a5ae727577230e38f28', '01521110903', '20200415110058.jpg', 1, '2020-04-15 21:00:58');
 
 -- --------------------------------------------------------
 
@@ -116,6 +122,7 @@ CREATE TABLE `student` (
   `email` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
+  `token` varchar(50) NOT NULL,
   `phone` varchar(11) NOT NULL,
   `image` varchar(50) DEFAULT NULL,
   `status` tinyint(1) NOT NULL,
@@ -126,8 +133,8 @@ CREATE TABLE `student` (
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `fname`, `roll`, `email`, `username`, `password`, `phone`, `image`, `status`, `datetime`) VALUES
-(17, 'proloy deb', 155750, 'sujoydas692@gmail.com', 'proloy456', '$2y$10$GNdojCz9kjJ05wb2S36JMevGGOUHQbTSQLoYdty4inl3Yq3nwm8EW', '01521110902', '20200412055118.jpg', 1, '2020-04-12 15:51:18');
+INSERT INTO `student` (`id`, `fname`, `roll`, `email`, `username`, `password`, `token`, `phone`, `image`, `status`, `datetime`) VALUES
+(24, 'Subir Das', 155750, 'sujoydas692@gmail.com', 'sujoy1234', '$2y$10$vG2xTXMFzdUd6Zc6zOsPIuHwYjhZEJhYo0F/8WzzJQPZW4VSuC.Si', 'f84a8273774e18db71125c1223347804', '01521110903', '20200415110453.jpg', 1, '2020-04-15 21:04:53');
 
 -- --------------------------------------------------------
 
@@ -195,25 +202,25 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT for table `issue_books`
 --
 ALTER TABLE `issue_books`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `librarian`
 --
 ALTER TABLE `librarian`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `token`
 --
 ALTER TABLE `token`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
